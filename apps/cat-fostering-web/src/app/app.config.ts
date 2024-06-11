abd16e00b088a18357fd1b504b4e9b87ff2ebff2
Edouard Maleix <ed@getlarge.eu>
2024-06-11 13:02:35 +0200

    feat(cat-fostering-web): define config providers

--- apps/cat-fostering-web/src/app/app.config.ts
+++ apps/cat-fostering-web/src/app/app.config.ts
@@ -1,8 +1,36 @@
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+  withXsrfConfiguration,
+} from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { environment } from '@cat-fostering/ng-env';
 
+import { provideApi, withBackendApiConfiguration } from './api.provider';
 import { appRoutes } from './app.routes';
 
+const XSRF_COOKIE_NAME = 'XSRF-TOKEN';
+const XSRF_HEADER_NAME = 'X-XSRF-TOKEN';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes)],
+  providers: [
+    provideRouter(appRoutes),
+    provideApi(
+      withBackendApiConfiguration({
+        basePath: environment.apiUrl,
+        withCredentials: true,
+        credentials: {
+          cookie: () => document.cookie,
+        },
+      })
+    ),
+    provideHttpClient(
+      withXsrfConfiguration({
+        cookieName: XSRF_COOKIE_NAME,
+        headerName: XSRF_HEADER_NAME,
+      }),
+      withInterceptorsFromDi()
+    ),
+  ],
 };
