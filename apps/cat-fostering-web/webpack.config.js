734f0cae8bbdc52b5c2b23090cf7681a6db5d373
Edouard Maleix <ed@getlarge.eu>
2024-08-24 10:26:24 +0200

    build(cat-fostering-web): tweak build config

--- apps/cat-fostering-web/webpack.config.js
+++ apps/cat-fostering-web/webpack.config.js
@@ -1,26 +1,37 @@
+const { join } = require('path');
 const { DefinePlugin } = require('webpack');
 
 const supportedEnvVariablesPattern = /^CAT_FOSTERING_|ORY_/i;
 
 function getClientEnvironment() {
-  const envVars = {
+  const env = {
     production: process.env.NODE_ENV === 'production',
   };
   for (const key in process.env) {
     if (supportedEnvVariablesPattern.test(key)) {
-      envVars[key] = process.env[key];
+      env[`process.env.${key}`] = JSON.stringify(process.env[key]);
     }
   }
-
-  return {
-    'process.env': JSON.stringify(envVars),
-  };
+  return env;
 }
 
-module.exports = (config, options, context) => {
-  // Overwrite the mode set by Angular if the NODE_ENV is set
-  config.mode = process.env.NODE_ENV || config.mode;
-  config.plugins ??= [];
-  config.plugins.push(new DefinePlugin(getClientEnvironment()));
-  return config;
+// module.exports = (config, options, context) => {
+//   // Overwrite the mode set by Angular if the NODE_ENV is set
+//   config.mode = process.env.NODE_ENV || config.mode;
+//   config.plugins ??= [];
+//   config.plugins.push(new DefinePlugin(getClientEnvironment()));
+//   return config;
+// };
+
+/**
+ * @type {import('webpack').Configuration}
+ */
+module.exports = {
+  mode: ['development', 'production'].includes(process.env.NODE_ENV)
+    ? process.env.NODE_ENV
+    : 'none',
+  output: {
+    path: join(__dirname, '../../dist/apps/cat-fostering-web'),
+  },
+  plugins: [new DefinePlugin(getClientEnvironment())],
 };
